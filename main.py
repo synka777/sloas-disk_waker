f13a60faaa092aad014805fe0b09d6356d4c9a56
Yegabebar <m.barbegayet@gmail.com>
2021-02-25 17:54:57 +0100

    Duplicates (files with the same name) management: Done - Created a function to delete orphan links

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ Released under the MIT license
 import os
 import shutil
 
-local_cache = {}
 local_files_cache = {}
 remote_files_cache = {}
+exclusion_list = {}
 
 
 def get_files(path):
@@ -32,8 +32,9 @@ def get_all_elements(path):
     files_names_cache = {}
     i = 0
     for file in os.listdir(path):
-        files_names_cache[i] = file
-        i += 1
+        if file not in exclusion_list:
+            files_names_cache[i] = file
+            i += 1
     return files_names_cache
 
 
@@ -49,13 +50,15 @@ def build_cache(path, file_list):
                   "st_ctime"]
     # Get the attributes of each file, then stores the couple (file name : dictionary_of_the_file_attributes) in the cache
     for file in file_list:
-        current_file_stats = os.stat(path + file)
-        stats_dict = {}
-        i = 0
-        for value in current_file_stats:
-            stats_dict[stat_names[i]] = value
-            i += 1
-        cache[file] = stats_dict
+        # If the file is not in the exclusion list we add it to the cache and return it
+        if file not in exclusion_list:
+            current_file_stats = os.stat(path + file)
+            stats_dict = {}
+            i = 0
+            for value in current_file_stats:
+                stats_dict[stat_names[i]] = value
+                i += 1
+            cache[file] = stats_dict
     return cache
 
 
@@ -80,15 +83,36 @@ def get_files_waiting_for_a_symlink(local, remote):
 
 
 def rename_duplicates(local_path, local, remote):
+    need_to_rebuild_cache = False
     for local_file in local:
         if local_file in remote:
             print("Duplicate found! : ", local_file)
-            # If the files don't have the same last edition date, we rename the local one to avoid a file override
+            # If the files don't have the same last edition date, we try to rename the local one to avoid a file override
             if remote[local_file]['st_mtime'] != local[local_file]['st_mtime']:
-                split = (local_file.split("."))
-                file_name = ".".join(split[0:-1])
-                file_type = split[-1]
-                os.rename(r''+str(local_path)+str(local_file), r''+str(local_path)+file_name+'.'+str(local[local_file]['st_mtime'])+"."+file_type)
+                try:
+                    split = (local_file.split("."))
+                    file_name = ".".join(split[0:-1])
+                    file_type = split[-1]
+                    os.rename(r''+str(local_path)+str(local_file), r''+str(local_path)+file_name+'.'+str(local[local_file]['st_mtime'])+"."+file_type)
+                except Exception as e:
+                    # If for some reason we can't rename the file, we put it on the exclusion list
+                    exclusion_list.update(local_file)
+                    print("Rename file: Error when parsing ", local_file, ": ", e)
+                finally:
+                    need_to_rebuild_cache = True
+    return need_to_rebuild_cache
+
+
+def delete_orphan_links(local_path, local, remote):
+    for file in local:
+        absolute_path_to_file = str(local_path)+str(local[file])
+        if os.path.islink(absolute_path_to_file) and file not in remote:
+            try:
+                os.remove(absolute_path_to_file)
+            except Exception as e:
+                # If for some reason we can't rename the file, we put it on the exclusion list
+                exclusion_list.update(file)
+                print("Delete orphan link: Error when parsing ", file, ": ", e)
 
 
 def get_file_list_to_copy(local, remote):
@@ -155,24 +179,22 @@ def main():
     local_file_list.update(get_files(local_path))
     remote_file_list.update(get_files(remote_path))
     # Builds some kind of log listing each file and it's attributes
-    remote_files_cache.update(build_cache(remote_path, remote_file_list))
-    # print("remote: ", remote_cache)
-
     local_files_cache.update((build_cache(local_path, local_file_list)))
-    # print("local: ", local_cache)
+    remote_files_cache.update(build_cache(remote_path, remote_file_list))
+
+    delete_orphan_links(local_path, get_all_elements(local_path), remote_files_cache)
 
     #
     # 1/ Manually started: check the difference between a local folder and it's remote counterpart
     #
 
-    # if local len > remote len, copy local file to remote
-    # else if local len < remote len, create a link in the local folder
-
-    rename_duplicates(local_path, local_files_cache, remote_files_cache)
-    # TODO:
-    # relancer la construcction d'un cache local avec les nouveaux fichiers renommés, ou :
-    # supprime du cache local l'entrée correspondant au fichier d'origine
-    # ajoute la nouvelle entrée au cache local
+    # Checks if some files exist on both locations and then tries to rename it.
+    # If it works for at least one file, we rebuild the cache with the current content from the local folder.
+    if rename_duplicates(local_path, local_files_cache, remote_files_cache):
+        local_file_list.clear()
+        local_files_cache.clear()
+        local_file_list.update(get_files(local_path))
+        local_files_cache.update((build_cache(local_path, local_file_list)))
 
     files_to_copy = {}
     files_to_copy = get_file_list_to_copy(local_files_cache, remote_files_cache)
@@ -191,14 +213,12 @@ def main():
     # 3/ Create a link to the local folder
     #
 
-    # Create a list of ALL files (symlinks included) with the name of each file and then put it in a cache
-    local_list = {}
-    local_list = get_all_elements(local_path)
-    local_cache.update(local_list)
+    # Create a list of ALL files names (w/ symlinks), then compare it with the remote folder's content
 
     waiting_for_a_symlink = {}
-    waiting_for_a_symlink = get_files_waiting_for_a_symlink(local_cache, remote_files_cache)
+    waiting_for_a_symlink = get_files_waiting_for_a_symlink(get_all_elements(local_path), remote_files_cache)
 
+    # Then pass the list of files that needs a symlink to the function that will create it
     mk_link_to_local(waiting_for_a_symlink, local_path, remote_path)
 
 
